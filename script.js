b1018db9d207a118761ba57b0c5f6b3db3cb3c18
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-02 13:34:47 -0500

    Display Results of each round
    The results of each round are sent to the console, along with a tallied score

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ function playRound() {
 
 // Get computer choice
 function getComputerChoice() {
-    choices = ['Rock', 'Paper', 'Scissors'];
-    return choices[Math.floor(Math.random() * 3)];
+    // choices: 'Rock', 'Paper', 'Scissors'
+    return Math.floor(Math.random() * 3)
 }
 
 // Get human choice
 function getHumanChoice() {
     let choice = prompt('Choose either "Rock", "Paper", or "Scissors"');
-    if (choice == 'Rock' || choice == 'Paper' || choice == 'Scissors') {
-        return choice;
+    if (choice == 'Rock') {
+        return 0;
+    }
+    else if (choice == 'Paper') {
+        return 1;
+    }
+    else if (choice == 'Scissors') {
+        return 2;
     }
     alert('Invalid input');
     getHumanChoice();
@@ -23,7 +29,23 @@ function getHumanChoice() {
 
 // Figure out who wins
 function displayWinner(computer, human) {
-
+    choices = ['Rock', 'Paper', 'Scissors'];
+    console.log(`Your choice: ${choices[human]}`);
+    console.log(`Computer choice: ${choices[computer]}`);
+    diff = (human - computer + 3) % 3;
+    if (diff == 0) {
+        console.log("You tied.");
+    }
+    else if (diff == 1) {
+        humanScore++;
+        console.log("You win!");
+    }
+    else {
+        computerScore++;
+        console.log("You lose...")
+    }
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer Score: ${computerScore}`)
 }
 
 // Play game
