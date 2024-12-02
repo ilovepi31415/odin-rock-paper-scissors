fe860ab77fadbd95121ce3bf840063b440422377
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-02 12:39:20 -0500

    Template basic functions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-// While playing (5 Rounds)
-
 // Get computer choice
+function getComputerChoice() {
 
+}
 // Get human choice
+function getHumanChoice() {
+
+}
 
 // Figure out who wins
+function displayWinner(computer, human) {
+
+}
+
+// While playing (5 rounds)
+for (i = 0; i < 5; i++) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+    winner = displayWinner(computerChoice, humanChoice);
+}
